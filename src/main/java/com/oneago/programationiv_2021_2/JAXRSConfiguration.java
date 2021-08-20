335d1930850923a2a1d71227e446996a78f41356
Juan David Rueda Quiroga <jrueda@oneago.com>
2021-08-19 19:16:38 -0500

    My first push

---
+++ src/main/java/com/oneago/programationiv_2021_2/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.oneago.programationiv_2021_2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
